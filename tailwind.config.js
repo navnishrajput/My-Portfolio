9c16a2ab1c08a8c0821933679a909d800fae69f6
navnishrajput <navneeshkumarrajput@gmail,com>
2026-01-23 09:14:34 +0530

    Force update to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Fixed path
   ],
+  
   theme: {
     extend: {
       keyframes: {
